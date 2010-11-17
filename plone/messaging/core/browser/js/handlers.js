146315345494481e5dccdb1d7fded88e4e879ac2
Yiorgis Gozadinos <ggozad@jarn.com>
2010-11-17 18:24:27 +0100

    Add some js order...

---
+++ plone/messaging/core/browser/js/handlers.js
@@ -0,0 +1,72 @@
+pmcxmpp.log = function (msg) 
+{
+	$('#log').append('<div></div><br/>').append(document.createTextNode(msg));
+}
+
+pmcxmpp.rawInput = function(data)
+{
+	pmcxmpp.log('RECV: ' + data);
+}
+
+pmcxmpp.rawOutput = function (data)
+{
+	pmcxmpp.log('SENT: ' + data);
+}
+
+pmcxmpp.Messages = {
+	gotMessage: function (message) {
+		var body = $(message).find('body').contents();
+		$(document).trigger('pmcxmpp.message', body);
+		return true;
+	}
+};
+
+pmcxmpp.Roster = {
+	rosterSet: function(iq) {
+		// XXX: Fill me in
+	},
+	rosterResult: function(iq) {
+		// XXX: Fill me in
+	}
+};
+
+pmcxmpp.Presence = {
+	presenceReceived: function (presence) {
+		var ptype = $(presence).attr('type');
+		var from = $(presence).attr('from');
+		var status = '';
+		if (ptype !== 'error') {
+			if (ptype === 'unavailable') {
+				status = 'offline';
+			} else {
+				var show = $(presence).find('show').text(); 
+				if (show === '' || show === '') {
+					status = 'online'
+				} else {
+					status = 'away'
+				}
+			} 
+		}
+		return true;
+	}
+};
+
+
+
+$(document).bind('pmcxmpp.connected', function () {
+	// Logging
+	pmcxmpp.connection.rawInput = pmcxmpp.rawInput;
+	pmcxmpp.connection.rawOutput = pmcxmpp.rawOutput;
+
+	// Messages
+	pmcxmpp.connection.addHandler(pmcxmpp.Messages.gotMessage,
+								  null, 'message', 'chat');
+	//Roster
+	pmcxmpp.connection.addHandler(pmcxmpp.Roster.rosterSet, Strophe.NS.ROSTER, 'iq', 'set');
+	pmcxmpp.connection.addHandler(pmcxmpp.Roster.rosterResult, Strophe.NS.ROSTER, 'iq', 'result');
+	//var roster_iq = $iq({type: 'get'}).c('query', {xmlns: Strophe.NS.ROSTER});
+	//pmcxmpp.connection.sendIQ(roster_iq);
+	// Presence
+	pmcxmpp.connection.addHandler(pmcxmpp.Presence.presenceReceived, null, 'presence', null);
+	pmcxmpp.connection.send($pres());
+});
--- plone/messaging/core/browser/js/messages.js
+++ plone/messaging/core/browser/js/messages.js
@@ -1,20 +1,12 @@
-pmcxmpp.Messages = {
-    gotMessage: function (message) {
-        var body = $(message).find('body').contents();
-        var div = $("<div></div>");
-        body.each(function () {
-            if (document.importNode) {
-                $(document.importNode(this, true)).appendTo(div);
-            } else {
-                // IE workaround
-                div.append(this.xml);
-            }
-        });
-        div.prependTo('#pcmxmpp-messages');
-        return true;
-    }
-};
-$(document).bind('pmcxmpp.connected', function () {
-    pmcxmpp.connection.addHandler(pmcxmpp.Messages.gotMessage,
-                                  null, 'message', 'chat');
+$(document).bind('pmcxmpp.message', function (event, message) {
+    var div = $('<div></div>');
+    $(message).each(function () {
+        if (document.importNode) {
+            $(document.importNode(this, true)).appendTo(div);
+        } else {
+            // IE workaround
+            div.append(this.xml);
+        }
+    });
+    $(div).prependTo('#pcmxmpp-messages');
 });
