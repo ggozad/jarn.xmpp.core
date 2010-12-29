cb27c43af8b138c829a76d5265d788a2b788a44a
Yiorgis Gozadinos <ggozad@jarn.com>
2010-12-29 22:41:36 +0100

    Create a collection node on setup. Add leaf node on user creation

---
+++ plone/messaging/core/setuphandlers.py
@@ -0,0 +1,10 @@
+from zope.component import getUtility
+
+from plone.messaging.core.interfaces import IPubSubClient
+
+
+def createDefaultPubSubNodes():
+    pubsub = getUtility(IPubSubClient)
+    pubsub.createNode('people',
+        options={'pubsub#node_title': 'All personal feeds',
+                 'pubsub#node_type': 'collection'})
--- plone/messaging/core/subscribers/user_management.py
+++ plone/messaging/core/subscribers/user_management.py
@@ -2,7 +2,7 @@ from zope.component import getUtility
 from plone.messaging.twisted.client import Admin
 from plone.messaging.twisted.interfaces import IDeferredXMPPClient
 from plone.messaging.core.interfaces import IXMPPSettings
-
+from plone.messaging.core.interfaces import IPubSubClient
 
 def onUserCreation(event):
     """Create a jabber account for new user.
@@ -13,17 +13,29 @@ def onUserCreation(event):
     jid = u'%s@%s' % (principal.getUserId(), jsettings.XMPPDomain)
     admin_jid = jsettings.getUserJID('admin')
     admin_password = jsettings.getUserPassword('admin')
+    pubsub = getUtility(IPubSubClient)
 
     def genPasswd():
+        return 'secret'
         import string
         import random
         chars = string.letters + string.digits
         return ''.join([random.choice(chars) for i in range(12)])
 
+    def addUserPubSubNode(add_result):
+        d = pubsub.createNode(principal.getUserId())
+        d.addCallback(configureUserPubSubNode)
+        return d
+
+    def configureUserPubSubNode(result):
+        d = pubsub.configureNode(principal.getUserId(), options={'pubsub#collection':'people'})
+        return d
+
     def addUser(xmlstream):
-        result = xmlstream.factory.streamManager. \
+        d = xmlstream.factory.streamManager. \
             handlers[0].addUser(jid, genPasswd())
-        return result
+        d.addCallback(addUserPubSubNode)
+        return d
 
     jabber_client = getUtility(IDeferredXMPPClient)
     d = jabber_client.execute(admin_jid, admin_password,
