25bcfb7a2f206b358f36c06951c415dbd4e718bf
Yiorgis Gozadinos <ggozad@jarn.com>
2011-01-26 18:09:02 +0100

    Show correct actor on content subscriber

--- plone/messaging/core/subscribers/content.py
+++ plone/messaging/core/subscribers/content.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from Products.CMFCore.interfaces import IActionSucceededEvent
+from Products.CMFCore.utils import getToolByName
 from twisted.words.xish.domish import Element
 from zope.component import getUtility
 from zope.lifecycleevent.interfaces import IObjectModifiedEvent
@@ -12,6 +13,8 @@ from plone.messaging.core.interfaces import IPubSubStorage
 
 def pubsubObjectModified(obj, event):
     uid = obj.UID()
+    pm = getToolByName(obj, 'portal_membership')
+    author = pm.getAuthenticatedMember().getId()
     message = ''
     if IObjectModifiedEvent.providedBy(event):
         message = "'%s' modified." % obj.Title()
@@ -19,7 +22,7 @@ def pubsubObjectModified(obj, event):
         message = "Workflow action '%s' on '%s'" % (event.action, obj.Title())
     entry = Element(('http://www.w3.org/2005/Atom', 'entry'))
     entry.addElement('content', content=message)
-    entry.addElement('author', content=obj.getOwner().getId())
+    entry.addElement('author', content=author)
     now = datetime.now().isoformat()
     entry.addElement('updated', content=now)
     entry.addElement('published', content=now)
