2303aed79372c797fe55510cc339b61357ebe249
Yiorgis Gozadinos <ggozad@jarn.com>
2011-01-28 16:59:59 +0100

    Click on user in online users portlet to start a chat

--- plone/messaging/core/browser/js/onlineusers.js
+++ plone/messaging/core/browser/js/onlineusers.js
@@ -1,8 +1,21 @@
 $(document).bind('pmcxmpp.presence', function (event, jid, status) {
-	jid = Strophe.getBareJidFromJid(jid);
-	existing = $('#online-users dd').find(":contains("+jid+")");
-	existing.parent().remove()
-	dd = $('<dd></dd>').addClass("portletItem");
-	dd.append($('<span></span>').addClass(status).text(jid));
-	$('#online-users').append(dd);
+    userid = Strophe.getNodeFromJid(jid);
+    barejid = Strophe.getBareJidFromJid(jid);
+    existing = $('#online-users dd').find(":contains("+userid+")");
+    existing.parent().remove()
+    dd = $('<dd></dd>').addClass("portletItem");
+    dd.append($('<button></button>')
+        .addClass(status)
+        .text('chat')
+        .attr('value', barejid));
+    dd.append($('<span></span>').addClass(status).text(userid));
+    $('#online-users').append(dd);
+});
+
+$(document).ready(function () {
+    $('button.online').live('click', function () {
+        var invitee = $(this).attr('value');
+        window.open(portal_url+'/@@muc?invitee='+invitee,
+                    "Chat", "menubar=0,resizable=0,width=800,height=500");
+    });
 });
