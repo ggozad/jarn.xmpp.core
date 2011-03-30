5f890cc7f9a756c24f88be6fab1145d3681ca73d
Yiorgis Gozadinos <ggozad@jarn.com>
2011-03-31 00:20:13 +0200

    Made presence portlet look nicer. Removed chat button.

--- jarn/xmpp/core/browser/js/onlineusers.js
+++ jarn/xmpp/core/browser/js/onlineusers.js
@@ -1,21 +1,27 @@
 $(document).bind('jarnxmpp.presence', function (event, jid, status) {
-    userid = Strophe.getNodeFromJid(jid);
-    barejid = Strophe.getBareJidFromJid(jid);
-    existing = $('#online-users dd').find(":contains("+userid+")");
-    existing.parent().remove()
-    dd = $('<dd></dd>').addClass("portletItem");
-    dd.append($('<button></button>')
-        .addClass(status)
-        .text('chat')
-        .attr('value', barejid));
-    dd.append($('<span></span>').addClass(status).text(userid));
-    $('#online-users').append(dd);
+    var userid = Strophe.getNodeFromJid(jid);
+    var barejid = Strophe.getBareJidFromJid(jid);
+    var existing = $('#online-users').find('#presence-portlet'+userid);
+    if (existing.length > 0) {
+        existing.attr('class', status);
+    } else {
+        member_info = $.getJSON(portal_url+"/xmpp-userinfo?user_id="+userid, function(data) {
+            var dd = $('<dd></dd>').attr('class', status).attr('id', 'presence-portlet'+userid);
+            dd.append($('<img/>').attr('title', data.fullname).attr('src', data.portrait_url));
+            $('#online-users').append(dd);
+        });
+    }
+    // dd.append($('<button></button>')
+    //     .addClass(status)
+    //     .text('chat')
+    //     .attr('value', barejid));
+    // dd.append($('<span></span>').addClass(status).text(userid));
 });
 
 $(document).ready(function () {
-    $('button.online').live('click', function () {
-        var invitee = $(this).attr('value');
-        window.open(portal_url+'/@@muc?invitee=' + invitee, "Chat",
-                    "menubar=0,resizable=0,width=800,height=500");
-    });
+    // $('button.online').live('click', function () {
+    //     var invitee = $(this).attr('value');
+    //     window.open(portal_url+'/@@muc?invitee=' + invitee, "Chat",
+    //                 "menubar=0,resizable=0,width=800,height=500");
+    // });
 });
