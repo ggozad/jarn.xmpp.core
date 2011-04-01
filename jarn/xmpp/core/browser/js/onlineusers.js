0a4fb12d4cf4b3b17ebf5482d7c095dc92bafb75
Yiorgis Gozadinos <ggozad@jarn.com>
2011-04-01 12:39:14 +0200

    Prepare online user actions

--- jarn/xmpp/core/browser/js/onlineusers.js
+++ jarn/xmpp/core/browser/js/onlineusers.js
@@ -1,27 +1,29 @@
 $(document).bind('jarnxmpp.presence', function (event, jid, status) {
     var userid = Strophe.getNodeFromJid(jid);
     var barejid = Strophe.getBareJidFromJid(jid);
-    var existing = $('#online-users').find('#presence-portlet'+userid);
+    var existing = $('#online-users').find('#online-users-'+userid);
     if (existing.length > 0) {
         existing.attr('class', status);
     } else {
         member_info = $.getJSON(portal_url+"/xmpp-userinfo?user_id="+userid, function(data) {
-            var dd = $('<dd></dd>').attr('class', status).attr('id', 'presence-portlet'+userid);
+            var dd = $('<dd></dd>').attr('class', status).attr('id', 'online-users-'+userid);
+            var ul = $('<ul class="online-users-actions inactive"><li class="online-users-message"><a>Message</a></li><ul>');
             dd.append($('<img/>').attr('title', data.fullname).attr('src', data.portrait_url));
+            dd.append(ul);
             $('#online-users').append(dd);
         });
     }
-    // dd.append($('<button></button>')
-    //     .addClass(status)
-    //     .text('chat')
-    //     .attr('value', barejid));
-    // dd.append($('<span></span>').addClass(status).text(userid));
 });
 
 $(document).ready(function () {
-    // $('button.online').live('click', function () {
-    //     var invitee = $(this).attr('value');
-    //     window.open(portal_url+'/@@muc?invitee=' + invitee, "Chat",
-    //                 "menubar=0,resizable=0,width=800,height=500");
-    // });
+    $('#online-users .online').live('hover', function () {
+        if (event.type === 'mouseover') {
+            $(this).find('.online-users-actions').attr('class', 'online-users-actions active');
+        } else {
+            $(this).find('.online-users-actions').attr('class', 'online-users-actions inactive');
+        }
+    });
+    $('.online-users-message').live('click', function() {
+        alert('message');
+    });
 });
