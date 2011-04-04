91c94003b766d3650df2a0ddedde2cf9e6f969e9
Yiorgis Gozadinos <ggozad@jarn.com>
2011-04-04 16:35:24 +0200

    Push chat window to an overlay

--- jarn/xmpp/core/browser/js/messages.js
+++ jarn/xmpp/core/browser/js/messages.js
@@ -1,10 +1,24 @@
 $(document).bind('jarnxmpp.message', function (event) {
     var userid = Strophe.getNodeFromJid(event.from);
+    var jid = Strophe.getBareJidFromJid(event.from);
+    var text_p = $('<p>').text(event.body);
+    var chat_p = $('<p>');
+    var chat_link = $('<a class="chat-link" href="muc?invitee=' + jid + '">Chat</a>');
+
+    chat_p.append(chat_link);
+    var text = $('<div>').append(text_p).append(chat_p).remove().html();
+
     var member_info = $.getJSON(portal_url+"/xmpp-userinfo?user_id="+userid, function(data) {
         $.gritter.add({
             title: data.fullname,
-            text: event.body,
-            image: data.portrait_url
+            text: text,
+            image: data.portrait_url,
+            sticky: true,
+            after_open: function (e) {
+                // e.find('.chat-link').prepOverlay({
+                //     subtype: 'ajax',
+                // });
+            },
         });
     });
 });
