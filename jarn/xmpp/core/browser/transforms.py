7c643933549cd424923a850430fe97e290e7c139
Yiorgis Gozadinos <ggozad@jarn.com>
2011-10-08 09:41:51 +0200

    Ram cache magic links

--- jarn/xmpp/core/browser/transforms.py
+++ jarn/xmpp/core/browser/transforms.py
@@ -2,49 +2,57 @@ import re
 import json
 import urllib2
 from urlparse import urlparse
-
 from BeautifulSoup import BeautifulSoup
+
+from plone.memoize import ram
 
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 
 
+@ram.cache(lambda method, url: url)
+def getURLData(url):
+    print "START %s"%url
+    try:
+        doc = urllib2.urlopen(url).read()
+    except urllib2.URLError:
+        return None
+    doc = BeautifulSoup(urllib2.urlopen(url).read())
+    title = u''
+    description = u''
+
+    # title
+    if doc.title:
+        title = doc.title.string
+    if not title:
+        title = doc.first('meta', attrs={'name': 'title'})
+        if title:
+            title = title.get('content')
+
+    # description
+    description = doc.first('meta', attrs={'name': 'description'})
+    if description:
+        description = description.get('content')
+
+    # Find favicon
+    favicon_url = doc.first('link', rel='shortcut icon')
+    if favicon_url:
+        favicon_url = favicon_url.get('href')
+    else:
+        host_url = urlparse(url)
+        favicon_url = host_url[0] + u'://' + host_url[1] + u'/favicon.ico'
+    print "END %s"%url
+
+    return json.dumps({
+        'title': title,
+        'description': description,
+        'favicon_url': favicon_url})
+
+
 class MagicLinksView(BrowserView):
 
     def __call__(self, url):
-        try:
-            doc = urllib2.urlopen(url).read()
-        except urllib2.URLError:
-            return None
-        doc = BeautifulSoup(urllib2.urlopen(url).read())
-        title = u''
-        description = u''
-
-        # title
-        if doc.title:
-            title = doc.title.string
-        if not title:
-            title = doc.first('meta', attrs={'name': 'title'})
-            if title:
-                title = title.get('content')
-
-        # description
-        description = doc.first('meta', attrs={'name': 'description'})
-        if description:
-            description = description.get('content')
-
-        # Find favicon
-        favicon_url = doc.first('link', rel='shortcut icon')
-        if favicon_url:
-            favicon_url = favicon_url.get('href')
-        else:
-            host_url = urlparse(url)
-            favicon_url = host_url[0] + u'://' + host_url[1] + u'/favicon.ico'
-
-        return json.dumps({
-            'title': title,
-            'description': description,
-            'favicon_url': favicon_url})
+        return getURLData(url)
 
 
 class ContentTransform(BrowserView):
