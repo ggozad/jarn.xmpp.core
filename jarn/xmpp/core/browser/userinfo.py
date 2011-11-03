a1af21a4cb89b7b8c08b7801904a04ecd8dfdb78
Yiorgis Gozadinos <ggozad@jarn.com>
2011-11-03 14:12:39 +0100

    For self show profile page link instead of message form

--- jarn/xmpp/core/browser/userinfo.py
+++ jarn/xmpp/core/browser/userinfo.py
@@ -50,3 +50,7 @@ class XMPPUserDetails(BrowserView):
     @property
     def portrait_url(self):
         return self._portrait_url
+
+    @property
+    def isSelf(self):
+        return self.user_id == self.pm.getAuthenticatedMember().getId()
