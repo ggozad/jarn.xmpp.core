3ea4840066102ab757a3dca715afe2a2ad3e123d
Yiorgis Gozadinos <ggozad@jarn.com>
2011-11-22 11:24:39 +0100

    Install jsi18n on upgrade

--- jarn/xmpp/core/upgrades/one_to_two.py
+++ jarn/xmpp/core/upgrades/one_to_two.py
@@ -10,9 +10,18 @@ def cleanJSRegistry(context):
         js_registry.unregisterResource('++resource++jarn.xmpp.core.js/strophe.pubsub.js')
     context.runImportStepFromProfile('profile-jarn.xmpp.core:default',
                                      'jsregistry')
+
+
+def updateActions(context):
     context.runImportStepFromProfile('profile-jarn.xmpp.core:default',
                                      'actions')
 
 
+def installJSi18n(context):
+    context.runAllImportStepsFromProfile('profile-jarn.jsi18n:default')
+
+
 def upgrade(context):
+    installJSi18n(context)
     cleanJSRegistry(context)
+    updateActions(context)
