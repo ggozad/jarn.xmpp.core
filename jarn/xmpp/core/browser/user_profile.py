52b7e1f8829fa1c9db0b3add129ccde9668581da
Yiorgis Gozadinos <ggozad@jarn.com>
2011-11-30 18:58:26 +0100

    Remove unecessary imports

--- jarn/xmpp/core/browser/user_profile.py
+++ jarn/xmpp/core/browser/user_profile.py
@@ -1,10 +1,4 @@
-import json
-
-from twisted.words.protocols.jabber.jid import JID
-
-from AccessControl import Unauthorized
 from Products.Five.browser import BrowserView
-from Products.CMFCore.utils import getToolByName
 
 
 class XMPPUserProfile(BrowserView):
